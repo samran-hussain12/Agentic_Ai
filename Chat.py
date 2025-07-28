2666089d811d3a5285b9de289f8b1b0c944eec6a
samran hussain <samranhussain67@gmail.com>
2025-07-28 14:20:08 +0500

    Additional Libraries

--- Chat.py
+++ Chat.py
@@ -2,3 +2,11 @@
 from langchain import OpenAI
 from langchain.chains import LLMChain
 from langchain.prompts import PromptTemplate
+# For document loading
+from langchain.document_loaders import TextLoader
+
+# For text splitting
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+
+# For retrieval
+from langchain.retrievers import VectorStoreRetriever
